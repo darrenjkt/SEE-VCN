ddaa5156b68d64252a13de52e88eeb69967d99fc
Darren <darrenjktsai@gmail.com>
2022-09-14 10:10:14 +1000

    setup.py changes

--- see/surface_completion/setup.py
+++ see/surface_completion/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 # pip install -e . --user
-setup(name='seev2',
+setup(name='see-vcn',
       version='1.0',
       description='Complete the surface of incomplete lidar point clouds',      
       author='Darren Tsai',
@@ -29,15 +29,15 @@ setup(name='seev2',
           CUDAExtension(
             name='chamfer', 
             sources=[
-              'models/partialsc/extensions/chamfer_dist/chamfer_cuda.cpp',
-              'models/partialsc/extensions/chamfer_dist/chamfer.cu',
+              'models/vcn/extensions/chamfer_dist/chamfer_cuda.cpp',
+              'models/vcn/extensions/chamfer_dist/chamfer.cu',
           ]),
           CUDAExtension(
             name='iou3d_nms_cuda', 
             sources=[
-              'models/partialsc/extensions/iou3d_nms/src/iou3d_cpu.cpp',
-              'models/partialsc/extensions/iou3d_nms/src/iou3d_nms_api.cpp',
-              'models/partialsc/extensions/iou3d_nms/src/iou3d_nms.cpp',
-              'models/partialsc/extensions/iou3d_nms/src/iou3d_nms_kernel.cu',
+              'models/vcn/extensions/iou3d_nms/src/iou3d_cpu.cpp',
+              'models/vcn/extensions/iou3d_nms/src/iou3d_nms_api.cpp',
+              'models/vcn/extensions/iou3d_nms/src/iou3d_nms.cpp',
+              'models/vcn/extensions/iou3d_nms/src/iou3d_nms_kernel.cu',
           ]),
       ])
