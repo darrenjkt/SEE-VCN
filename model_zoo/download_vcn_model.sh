63384cfd2ea9885213de1e79133a460f6799750d
Darren Tsai <39115809+darrenjkt@users.noreply.github.com>
2022-09-17 17:57:09 +1000

    Add files via upload

---
+++ model_zoo/download_vcn_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-z0cfttgnd_GirdnPgVkmpms69MtWOzp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-z0cfttgnd_GirdnPgVkmpms69MtWOzp" -O VCN_VC.pth && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1w4l0r33dTzy3KGoF5BfPhQLFiRFmm4iR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1w4l0r33dTzy3KGoF5BfPhQLFiRFmm4iR" -O VCN_CN.pth && rm -rf /tmp/cookies.txt
